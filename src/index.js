af6e2a3d97cca7f27f72ad1645840463b6de84f7
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-05-11 16:10:13 -0400

    Create basic layout/card element and stylings Write basic pseudocode for project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+//IMPORTS
+import "./styles.css";
+
+
+
+// We need the async function to fetch from the APIkey 
+// Need a DOM creation module to handle creating the content inside of the weather card
+// Need a ui-controller module to bind form submission event
+// 
+
+
+
+
+//dynamically change the background of the card based on what the current weather status is to reflect the theme of that weather.
+//(So if the weather is sunny, it shoulder be a blue-yellow gradient, whereas if cloudy, a grey-blue gradient perhaps?)
+
+//Make the temperature conversion button a slider button so it sits between a celsius and farhenheit option and slides back and forth
+
+//Check if the object the API returns contains time information. If not, remove the time from the data. (OR maybe try fetching time with a separate API?)
+
+//The default area for the weather to be displayed will always be New York. 
+
+//Update the icon dynamically to reflect the current weather status as well based on what the API returns. 
+
